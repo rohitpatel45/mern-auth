ce9e2d46fbfe6abd2c8f83fe30252887743c71b4
SonnySanga <rohit.kumar018219>
2024-03-10 07:30:51 +0530

    add header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,10 +8,12 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
