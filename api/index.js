242097b664c00b8e021c4cb469cb80d465f69293
SonnySanga <rohit.kumar018219>
2024-03-10 10:51:59 +0530

    added signup functionaltiy in frinent

--- api/index.js
+++ api/index.js
@@ -32,7 +32,7 @@ app.use((error, req, res, next) =>{
 const statusCode = error.statusCode || 500;
 const message = error.message || "Internal Server Error"
 return res.status(statusCode).json({
-    success: true,
+    success: false,
     message,
     statusCode
 })
